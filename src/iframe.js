6d0bbf1e8801a6ba40a87b6310e0184992bf1e4e
Silviu Iordache <silviu.iordache@caphyon.com>
2021-09-10 16:55:54 +0300

    Remove commented line

--- src/iframe.js
+++ src/iframe.js
@@ -1,6 +1,6 @@
 function sendMessageToParent() {
   const message = document.getElementById('iFrameMessage').value;
-  // const targetOrigin = 'http://127.0.0.1:5500/index.html';
-  const targetOrigin = window.parent.location.origin
+  //grab url of parent window
+  const targetOrigin = window.parent.location.origin;
   window.parent.postMessage(message, targetOrigin);
 }
