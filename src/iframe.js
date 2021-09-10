2d24ddc0f801485a096196dbfa12cea600e60fc4
Silviu Iordache <silviu.iordache@caphyon.com>
2021-09-10 16:55:06 +0300

    Adapt target origin from window.parent

--- src/iframe.js
+++ src/iframe.js
@@ -1,5 +1,6 @@
 function sendMessageToParent() {
   const message = document.getElementById('iFrameMessage').value;
-  const targetOrigin = 'http://127.0.0.1:5500/index.html';
-  window.parent.postMessage(message, targetOrigin);   
+  // const targetOrigin = 'http://127.0.0.1:5500/index.html';
+  const targetOrigin = window.parent.location.origin
+  window.parent.postMessage(message, targetOrigin);
 }
